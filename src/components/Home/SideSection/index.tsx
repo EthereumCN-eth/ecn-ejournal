f121a15310dfb3fe6dee3784bceffc46838913b0
kent426 <kenthuang426@gmail.com>
2023-01-30 14:50:54 +0800

    feat(components): rss btn

--- src/components/Home/SideSection/index.tsx
+++ src/components/Home/SideSection/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-nested-ternary */
-import { Box } from "@chakra-ui/react";
+import { Button, Flex, Image } from "@chakra-ui/react";
 import { css } from "@emotion/react";
 import { useAtom } from "jotai";
 
@@ -15,25 +15,45 @@ export const SideSection = () => {
   // console.log("isOpen", isOpen);
 
   return (
-    <>
-      <Box
-        css={css`
-          ${isLargerW &&
-          css`
-            position: sticky;
-            top: 20px;
-          `}
+    <Flex
+      css={css`
+        ${isLargerW &&
+        css`
+          position: sticky;
+          top: 20px;
         `}
-        opacity={isOpen ? 1 : 0}
-        zIndex={isOpen ? 1 : -1}
-        transition={isOpen ? "opacity 2s ease 1.7s" : ""}
-      >
-        {/* {" "} */}
-        <DatePickerView />
-      </Box>
-      {/* <FloatBtn /> */}
+      `}
+      direction="column"
+      opacity={isOpen ? 1 : 0}
+      zIndex={isOpen ? 1 : -1}
+      transition={isOpen ? "opacity 2s ease 1.7s" : ""}
+    >
+      {/* {" "} */}
+      <DatePickerView />
 
+      {/* <FloatBtn /> */}
+      <Button
+        mt="20px"
+        w="full"
+        variant="join"
+        color="#fff"
+        px="74px"
+        py="10px"
+        aria-label="rs-btn"
+        minH="48px"
+        css={css`
+          font-family: "PingFang SC";
+          font-style: normal;
+          font-weight: 600;
+          font-size: 1rem;
+          line-height: 1.37rem;
+        `}
+        cursor="pointer"
+        leftIcon={<Image alt="rss" src="/rss.svg" w="24px" h="24px" />}
+      >
+        订阅 E 群誌
+      </Button>
       {/*  */}
-    </>
+    </Flex>
   );
 };
--- src/styles/theme/components/button.ts
+++ src/styles/theme/components/button.ts
@@ -9,6 +9,21 @@ export const Button: ComponentStyleConfig = {
     join: {
       bgColor: "#3D3D3D",
       borderRadius: "8px",
+
+      _hover: {
+        bgColor: lighten(0.1, "#3D3D3D"),
+        _disabled: {
+          bgColor: "gray.300",
+        },
+      },
+      _active: {
+        bgColor: lighten(0.2, "#3D3D3D"),
+      },
+    },
+    rss: {
+      bgColor: "#3D3D3D",
+      borderRadius: "8px",
+
       _hover: {
         bgColor: lighten(0.1, "#3D3D3D"),
         _disabled: {
