17ca28e2439eb7dd67fcbc40c67b8bf0f8897a48
kent426 <kenthuang426@gmail.com>
2023-02-20 18:49:06 +0800

    feat(components): footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,75 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Link, HStack, Divider, IconButton } from "@chakra-ui/react";
+import { AiFillWechat } from "react-icons/ai";
+import { FaDiscord, FaTwitter } from "react-icons/fa";
 
+const SmallWithNavigation = () => {
+  return (
+    <Flex width="full">
+      <HStack
+        // minH="308px"
+        spacing={6}
+        color="white"
+      >
+        <Link target="_blank" href="http://ethereum.cn/">
+          ETHEREUM.CN
+        </Link>
+        <Link target="_blank" href="https://ethgifts.com/">
+          ETHGIFTS
+        </Link>
+        <Link target="_blank" href="https://ecnpodcast.fireside.fm/">
+          ECN PODCAST
+        </Link>
+        <Link target="_blank" href="https://devcon.app/">
+          DEVCON.APP
+        </Link>
+      </HStack>
+    </Flex>
+  );
+};
 const Footer = () => {
   return (
-    <Flex as="footer" width="full" justifyContent="center">
-      {/*  */}
+    <Flex
+      as="footer"
+      width="full"
+      justifyContent="center"
+      maxW="1200px"
+      p="40px"
+      direction="row"
+      position="relative"
+    >
+      <Divider
+        position="absolute"
+        top="0"
+        maxW="1200px"
+        w="calc(100% - 80px)"
+      />
+      <SmallWithNavigation />
+      <HStack spacing={6}>
+        <IconButton
+          onClick={() => {
+            window.open("https://twitter.com/EthereumCN", "_blank");
+          }}
+          aria-label="twitter"
+          icon={<FaTwitter />}
+        />
+        <IconButton
+          onClick={() => {
+            window.open("https://discord.gg/KUywx3JJJU", "_blank");
+          }}
+          aria-label="discord"
+          icon={<FaDiscord />}
+        />
+        <IconButton
+          onClick={() => {
+            window.open(
+              "https://i.ibb.co/mBgmDgF/footer-wechat.webp",
+              "_blank"
+            );
+          }}
+          aria-label="wechat"
+          icon={<AiFillWechat />}
+        />
+      </HStack>
     </Flex>
   );
 };
